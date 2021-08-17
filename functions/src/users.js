fc5fb2b602a1e5235ab1d5ca37ef11800b13737c
Todd Albert <todd@bocacode.com>
2021-08-17 11:49:56 -0400

    added JWT and returning token

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,5 +1,7 @@
 const admin = require('firebase-admin')
+const jwt = require('jsonwebtoken')
 const creds = require('../credentials.json')
+const { secret } = require('../secrets')
 
 function connectDb() {
   if(!admin.apps.length) { // we haven't already connected...
@@ -27,10 +29,12 @@ exports.userSignup = (req, res) => {
     .doc(req.body.email.toLowerCase())
     .set(req.body)
     .then(() => {
+      const token = jwt.sign({ email: req.body.email }, secret)
       res.send({
         message: 'User created successfully',
         status: 200,
-        success: true
+        success: true,
+        token
       })
     })
     .catch(err => {
@@ -43,6 +47,48 @@ exports.userSignup = (req, res) => {
 }
 
 exports.userLogin = (req, res) => {
-  res.send('ok')
+  // check that email and password present in the request
+  if(!req.body || !req.body.email || !req.body.password) {
+    res.status(400).send({
+      message: 'Invalid Request: missing email or password',
+      status: 400,
+      success: false
+    })
+    return
+  }
+  // connect to the database
+  const db = connectDb()
+  // check to see if user exists with email and password
+  db.collection('users')
+    .where('email', '==', req.body.email.toLowerCase())
+    .where('password', '==', req.body.password)
+    .get()
+    .then(userCollection => {
+      if(userCollection.docs.length) {
+        let user = userCollection.docs[0].data()
+        user.password = undefined
+        // create a token for the user
+        const token = jwt.sign(user, secret)
+        res.send({
+          message: 'User logged in successfully',
+          status: 200,
+          success: true,
+          token
+        })
+      } else {
+        res.status(401).send({
+          message: 'Invalid email or password',
+          status: 401,
+          success: false
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'Error: ' + err.message,
+        status: 500,
+        success: false
+      })
+    })
 }
 
